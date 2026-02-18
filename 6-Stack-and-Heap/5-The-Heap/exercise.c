aa77a53efe15cd1c9d6304116511ad60f02d757e
abdullah salameh <79572435+abda-s@users.noreply.github.com>
2026-02-18 11:38:30 +0000

    Doc: comment explaining

--- 6-Stack-and-Heap/5-The-Heap/exercise.c
+++ 6-Stack-and-Heap/5-The-Heap/exercise.c
@@ -4,6 +4,6 @@
 
 char *get_full_greeting(char *greeting, char *name, int size) {
   char *full_greeting = malloc(size);
-  snprintf(full_greeting, size, "%s %s", greeting, name);
+  snprintf(full_greeting, size, "%s %s", greeting, name); // this functions writes to the Heap memory adr it just needs the adr and the values 
   return full_greeting;
 }
